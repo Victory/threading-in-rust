2c332e48f5ce203420c84dc98034ad3992dfdba7
Victory <git@dfhu.org>
2014-11-12 19:58:15 -0500

    add some delay

--- threading/src/main.rs
+++ threading/src/main.rs
@@ -1,19 +1,19 @@
 extern crate native;
 
 use std::comm;
-
+use std::io::timer;
+use std::time::duration::Duration;
 
 fn main() {
     let (tx, rx): (Sender<uint>, Receiver<uint>) = comm::channel();
 
-
-
     for ii in range(0u, 10) {
+        let interval = Duration::milliseconds(ii as i64 * 20i64);
         let task_tx = tx.clone(); // clone the Sender
 
         spawn(proc() {
             task_tx.send(ii);
-
+            timer::sleep(interval);
             println!("task {} is done", ii);
         });
     }
