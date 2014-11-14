8d5dd8f64980a4a3b31168a1c33ec0818f0eddf0
Victory <git@dfhu.org>
2014-11-13 19:54:12 -0500

    howdy server

--- unix-sockets/src/main.rs
+++ unix-sockets/src/main.rs
@@ -1,3 +1,31 @@
+use std::io::{TcpListener, TcpStream};
+use std::io::{Acceptor, Listener};
+use std::io::BufferedStream;
+
 fn main () {
 
+    let listener = TcpListener::bind("127.0.0.1", 8099).unwrap();
+    let mut acceptor = listener.listen().unwrap();
+    
+    fn handle_client(mut stream: BufferedStream<TcpStream>) {
+        stream.write(b"HTTP/1.1 200 OK\n"); // byte literal
+        stream.write(b"Content-length: 15\n"); // byte literal
+        stream.write(b"Content-type: text/html\n"); // byte literal
+        stream.write(b"\n\n <p>Howdy!</p>");
+        //stream.flush();
+
+        println!("Handling acceptor");
+    }
+
+    for stream in acceptor.incoming() {
+        match stream {
+            Err(e) => { println!("connection failed: {}", e) }
+            Ok(mut stream) => spawn(proc() {
+                let mut bs = BufferedStream::new(stream);
+                handle_client(bs)
+            })
+        }
+    }
+
+    drop(acceptor);
 }
