e5bf0b27c553a66708b952d93f8cd10f739cb8e3
Victory <git@dfhu.org>
2014-12-05 19:12:43 -0500

    remove unneeded as_slice()

--- unix-sockets/src/main.rs
+++ unix-sockets/src/main.rs
@@ -287,9 +287,9 @@ fn main () {
         stream.write(b"HTTP/1.1 200 OK\r\n").unwrap();
         stream.write(b"Content-type: text/html\r\n").unwrap();
         stream.write(b"X-header: from bytes\r\n").unwrap();
-        stream.write(body_length.into_bytes().as_slice()).unwrap();
+        stream.write(body_length.as_bytes()).unwrap();
         stream.write(b"\r\n\r\n").unwrap();
-        stream.write(body.into_bytes().as_slice()).unwrap();
+        stream.write(body.as_bytes()).unwrap();
         stream.flush().unwrap();
 
         println!("Done handling acceptor");
